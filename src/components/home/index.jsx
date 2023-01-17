18df38081aa76d7df08eb5d6e53327913ecff770
Tiago Lopes <saxtiago14@gmail.com>
2023-01-16 22:18:18 -0300

    Feat - component home created

---
+++ src/components/home/index.jsx
@@ -0,0 +1,37 @@
+import './App.css';
+import React from 'react';
+import { Button } from '../buttons';
+
+export const Home = () => {
+  return (
+    <>
+      <Button 
+        textDisplay={'Clique aqui'}
+      />
+      <div>
+        <p>Pesquise</p>
+        <input></input>
+      </div>
+      <Button 
+        textDisplay={'Reiniciar'}
+      />
+    </>
+  );
+};
+
+// <div className="App">
+//   <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <p>
+//       Edit <code>src/App.js</code> and save to reload.
+//     </p>
+//     <a
+//       className="App-link"
+//       href="https://reactjs.org"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//     >
+//       Learn React
+//     </a>
+//   </header>
+// </div>
